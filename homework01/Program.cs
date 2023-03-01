4afc9ba6451118c425c92afc526aafcf9ff8fe21
Valentina <valyapomortseva@gmail.com>
2023-03-01 18:21:10 +0700

    Начала писать через метод задачу 1

--- homework01/Program.cs
+++ homework01/Program.cs
@@ -1,3 +1,9 @@
-﻿Console.WriteLine("Введите число N: ");
-int N = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine(N*N);
+﻿int result = NewMethod();
+Console.WriteLine(N * N);
+
+int NewMethod(string s)
+{
+    Console.WriteLine("s");
+    int N = Convert.ToInt32(Console.ReadLine());
+    return N;
+}
